e61727c32ca759c3c4786908c39fae1dc328cfc9
sfimbres <stevenfimbres22@gmail.com>
2022-02-09 13:05:17 -0800

    Update Scoreboard Logic

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+/*
+Strikezone:
+[00][01] = [0][1]
+[10][11]   [2][3]
+
+Pitches:
+FB : 0
+CB : 1
+CH : 2
+SL : 3
+
+Bases:
+                [2]
+[3, 2, 1] => [3]   [1]
+
+false = not on base
+true = on base
+*/
+
+
 //initialize variables
 let homeScore = 0;
 let awayScore = 0;
 let inning = 1;
+let balls = 0;
+let strikes = 0;
 let outs = 0;
 let isBottomInning = false;
 let is9thInning = false;
-let bases = [0,0,0];
+let bases = [false, false, true];
 let gameOver = false;
+let zoneSquare = -1;
+let currentPitch = -1;
 
 //hitting parameters
 //pitching parameters
@@ -16,6 +40,33 @@ let gameOver = false;
 //pitching logic (cpu and guess pitch)
 
 //probably logic for moving runners
+
+//
+
+//update score function (based on home and away score)
+const updateScore = () => {
+    document.querySelector("#away-score").innerHTML = awayScore;
+    document.querySelector("#home-score").innerHTML = homeScore;
+};
+
+//update bases function
+const updateBases = () => {
+    if(bases[0] === true){
+        document.querySelector("#base1").style.backgroundColor = "rgb(247, 211, 6)";
+    }
+    if(bases[1] === true){
+        document.querySelector("#base2").style.backgroundColor = "rgb(247, 211, 6)";
+    }
+    if(bases[2] === true){
+        document.querySelector("#base3").style.backgroundColor = "rgb(247, 211, 6)";
+    }
+}
+
+//function to update count
+const updateCount = () => {
+    document.querySelector("#balls").innerHTML = balls;
+    document.querySelector("#strikes").innerHTML = strikes;
+}
 
 //check if game is over
 const isGameOver = () => {
@@ -27,23 +78,27 @@ const resetGame = () => {
     homeScore = 0;
     awayScore = 0;
     inning = 1;
+    balls = 0;
+    strikes = 0;
     outs = 0;
     isBottomInning = false;
     is9thInning = false;
-    bases = [0,0,0];
+    bases = [false,false,false];
     gameOver = false;
+    zoneSquare = -1;
+    currentPitch = -1;
 };
 
 //function to play game
 const game = () => {
-
+    updateCount();
     //check game over
-    while(is9thInning === false && gameOver === false){
+    /*while(is9thInning === false && gameOver === false){
 
         //pitch
         if(isBottomInning === false){
             while (outs < 3 && gameOver === false){
-                //change to pitvh interface
+                //change to pitch interface
                 //pitch logic
             }
         }
@@ -54,7 +109,7 @@ const game = () => {
             }
         }
         //display game over when game is over
-    }
+    }*/
 };
 
 //play game
