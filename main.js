3c4b365b831c3fadb4e8b132fafe5ca20ef3b461
sfimbres <stevenfimbres22@gmail.com>
2022-02-09 22:05:22 -0800

    Update Baseball Logic

--- main.js
+++ main.js
@@ -27,21 +27,94 @@ let strikes = 0;
 let outs = 0;
 let isBottomInning = false;
 let is9thInning = false;
-let bases = [false, false, true];
+let bases = [false, false, false];
 let gameOver = false;
 let zoneSquare = -1;
 let currentPitch = -1;
 
-//hitting parameters
-//pitching parameters
+//hitting parameters (if nec)
+//pitching parameters (if nec)
 
 //hitting logic (cpu and guess pitch)
-
+const cpuGuessPitch = () => {}
+const cpuGuessPitchLocation = () => {}
 //pitching logic (cpu and guess pitch)
+const cpuGetPitch = () => {}
+const cpuGetPitchLocation = () => {}
 
-//probably logic for moving runners
+//function to move runners and update score on single
+const onSingle = () => {
+  for(let i = 0; i < 3; i++){
+      if(bases[i] === true){
+          if(i === 0){
+              //check team and update correct score
+              (isbottomInning === false) ? awayScore++ : homeScore++;
+              updateScore();
+              bases[i] = false;
+          }
+          else{
+              bases[i] = false;
+              bases[i-1] = true;
+          }
+      }
+  }
 
-//
+  bases[2] = true;
+}
+
+//function to move runners and update score on double
+const onDouble = () => {
+    for(let i = 0; i < 3; i++){
+        if(bases[i] === true){
+            if(i <= 1){
+                //check team and update correct score
+                (isbottomInning === false) ? awayScore++ : homeScore++;
+                updateScore();
+                bases[i] = false;
+            }
+            else{
+                bases[i] = false;
+                if(i=2)
+                bases[i-2] = true;
+            }
+        }
+    }
+  
+    bases[1] = true;
+}
+
+//function to move runners and update score on triple
+const onTriple = () => {
+    for(let i = 0; i < 3; i++){
+        if(bases[i] === true){
+            if(i <= 1){
+                //check team and update correct score
+                (isbottomInning === false) ? awayScore++ : homeScore++;
+                updateScore();
+                bases[i] = false;
+            }
+        }
+    }
+  
+    bases[3] = true;
+}
+
+//function to move runners and update score on homerun
+const onHR = () => {
+    for(let i = 0; i < 3; i++){
+        if(bases[i] === true){
+            if(i <= 1){
+                //check team and update correct score
+                (isbottomInning === false) ? awayScore++ : homeScore++;
+                updateScore();
+                bases[i] = false;
+            }
+        }
+    }
+    //add one more run and update score
+    (isbottomInning === false) ? awayScore++ : homeScore++;
+    updateScore();
+}
 
 //update score function (based on home and away score)
 const updateScore = () => {
@@ -89,28 +162,216 @@ const resetGame = () => {
     currentPitch = -1;
 };
 
-//function to play game
-const game = () => {
-    updateCount();
-    //check game over
-    /*while(is9thInning === false && gameOver === false){
-
-        //pitch
-        if(isBottomInning === false){
-            while (outs < 3 && gameOver === false){
-                //change to pitch interface
-                //pitch logic
-            }
-        }
-        else{
-            while (outs < 3 && gameOver === false){
-                //change to hit interface
-                //hit logic
-            }
-        }
-        //display game over when game is over
-    }*/
+//reset count
+const resetCount = () => {
+    balls = 0;
+    strikes = 0;
 };
 
+//reset outs
+const resetOuts = () => {
+    outs = 0;
+}
+
+//reset bases
+const resetBases = () => {
+    bases = [false, false, false];
+};
+
+//new inning
+const newInning = () => {
+    resetCount();
+    resetOuts();
+    resetBases();
+
+    //update inning
+    if(isBottomInning === false){
+        isBottomInning = true;
+        //flip triangle
+        document.querySelectorAll("#inning-indicator").classList.toggle("transpose");
+    }
+    else{
+        inning++;
+        isBottomInning = false;
+        document.querySelectorAll("#inning-number").innerHTML = inning;
+        document.querySelectorAll("#inning-indicator").classList.toggle("transpose");
+    }
+};
+
+//enable pitch buttons
+const enablePitches = () => {
+    let pitchesArray = document.querySelectorAll(".pitch");
+
+    pitchesArray.forEach(btn => {
+        btn.disabled = false;
+    });
+}
+
+//enable pitch buttons
+const disablePitches = () => {
+    let pitchesArray = document.querySelectorAll(".pitch");
+
+    pitchesArray.forEach(btn => {
+        btn.disabled = true;
+        btn.style.backgroundColor = "white";
+    });
+}
+
+//enable strikezone buttons
+const enableStrikezone = () => {
+    let zoneArray = document.querySelectorAll(".square-zone");
+
+    zoneArray.forEach(btn => {
+        btn.disabled = false;
+        btn.style.backgroundColor = "white";
+    });
+}
+
+//enable strikezone buttons
+const disableStrikezone = () => {
+    let zoneArray = document.querySelectorAll(".square-zone");
+
+    zoneArray.forEach(btn => {
+        btn.disabled = true;
+    });
+}
+
+//click strikezone zone 1
+const onClickTopLeft = () => {
+    zoneSquare = 0;
+    enablePitches();
+    document.querySelector("#zone1").style.backgroundColor = "red";
+    disableStrikezone();
+};
+
+//click strikezone zone 2
+const onClickTopRight = () => {
+    zoneSquare = 1;
+    enablePitches();
+    document.querySelector("#zone2").style.backgroundColor = "red";
+    disableStrikezone();
+};
+
+//click strikezone zone 3
+const onClickBottomLeft = () => {
+    zoneSquare = 2;
+    enablePitches();
+    document.querySelector("#zone3").style.backgroundColor = "red";
+    disableStrikezone();
+};
+
+//click strikezone zone 3
+const onClickBottomRight = () => {
+    zoneSquare = 3;
+    enablePitches();
+    document.querySelector("#zone4").style.backgroundColor = "red";
+    disableStrikezone();
+};
+
+//click FB button
+const onClickFB = () => {
+    currentPitch = 0;
+    disablePitches();
+    document.querySelector("#fb").style.backgroundColor = "red";
+    //function to play game
+    //calcBatResult (zoneSquare, currentPitch)
+    //or
+    //calcPitchResult (zoneSquare, currentPitch)
+    //will compared to cpu generated values and decide if ball, strike, hit, or out
+    //will have to modify board depending omn result
+    enableStrikezone();
+    disablePitches();
+};
+
+//click CB button
+const onClickCV = () => {
+    currentPitch = 1;
+    disablePitches();
+    document.querySelector("#cv").style.backgroundColor = "red";
+    //function to play game
+    //calcBatResult (zoneSquare, currentPitch)
+    //or
+    //calcPitchResult (zoneSquare, currentPitch)
+    //will compared to cpu generated values and decide if ball, strike, hit, or out
+    //will have to modify board depending omn result
+    enableStrikezone();
+    disablePitches();
+};
+
+//click CH button
+const onClickCH = () => {
+    currentPitch = 1;
+    disablePitches();
+    document.querySelector("#ch").style.backgroundColor = "red";
+    //function to play game
+    //calcBatResult (zoneSquare, currentPitch)
+    //or
+    //calcPitchResult (zoneSquare, currentPitch)
+    //will compared to cpu generated values and decide if ball, strike, hit, or out
+    //will have to modify board depending omn result
+    enableStrikezone();
+    disablePitches();
+};
+
+//click SL button
+const onClickSL = () => {
+    currentPitch = 1;
+    disablePitches();
+    document.querySelector("#sl").style.backgroundColor = "red";
+    //function to play game
+    //calcBatResult (zoneSquare, currentPitch)
+    //or
+    //calcPitchResult (zoneSquare, currentPitch)
+    //will compared to cpu generated values and decide if ball, strike, hit, or out
+    //will have to modify board depending omn result
+    enableStrikezone();
+    disablePitches();
+};
+
+//function to play game
+const game = () => {
+    
+    //pitching
+    if(isBottomInning === false){
+        document.querySelector("#statement").innerHTML = "You are Pitching";
+        document.querySelector("#statement2").innerHTML = "New Half Inning";
+        while (outs < 3 && gameOver === false){
+            //change to pitch interface
+            //pitch logic
+            
+            //select pitches
+            //
+        }
+    }
+    //batting
+    else{
+        document.querySelector("#statement").innerHTML = "You are Pitching";
+        document.querySelector("#statement2").innerHTML = "New Half Inning";
+        while (outs < 3 && gameOver === false){
+            //change to hit interface
+            //hit logic
+
+        }
+    }
+
+    
+    //display game over when game is over
+   
+};
+
+const main = () => {
+    document.querySelector("#zone1").addEventListener("mouseup", () => {onClickTopLeft()});
+    document.querySelector("#zone2").addEventListener("mouseup", () => {onClickTopRight()});
+    document.querySelector("#zone3").addEventListener("mouseup", () => {onClickBottomLeft()});
+    document.querySelector("#zone4").addEventListener("mouseup", () => {onClickBottomRight()});
+    document.querySelector("#fb").addEventListener("mouseup", () => {onClickFB()});
+    document.querySelector("#cv").addEventListener("mouseup", () => {onClickCV()});
+    document.querySelector("#ch").addEventListener("mouseup", () => {onClickCH()});
+    document.querySelector("#sl").addEventListener("mouseup", () => {onClickSL()});
+
+    //game();
+}
+
 //play game
-game();
+//game();
+main();
